7364e0b87f0b72b64c8a977468f75e8dfd4adaf2
Duyan Kim <46421950+duyankim@users.noreply.github.com>
2021-06-12 23:51:11 +0900

    [feat] **DB02**: `toggle.js` add toggle click event to `menu.html`

--- src/DB02/script/toggle.js
+++ src/DB02/script/toggle.js
@@ -1,7 +1,23 @@
 const toggleArrow = (icon) => {
+    const menu1 = document.getElementById("menuDetail1");
+    const menu2 = document.getElementById("menuDetail2");
+    const menu3 = document.getElementById("menuDetail3");
+    const menus = [menu1, menu2, menu3];
+    const menusStr = ["menu1", "menu2", "menu3"];
+
     if (icon.classList.contains("arrowOpen")) {
         icon.classList.remove("arrowOpen");
+        for (let i = 0; i < menus.length; i++) {
+            if (icon.id == menusStr[i]) {
+                menus[i].classList.add("menuDisplayNone");
+            }
+        }
     } else {
         icon.classList.add("arrowOpen");
+        for (let i = 0; i < menus.length; i++) {
+            if (icon.id == menusStr[i]) {
+                menus[i].classList.remove("menuDisplayNone");
+            }
+        }
     }
 }
