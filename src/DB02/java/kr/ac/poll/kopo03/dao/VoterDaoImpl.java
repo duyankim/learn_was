9fdc3230904c9d6f807919c4d11440c8cd199b70
Duyan Kim <46421950+duyankim@users.noreply.github.com>
2021-06-17 20:52:42 +0900

    [feat] **DB02**: `VoterDaoImpl.java` add method `totalVotesCntOfOneCandidate`, `totalVotesCntOfAllCandidates`, `votersListOfOneCandidate`

--- src/DB02/java/kr/ac/poll/kopo03/dao/VoterDaoImpl.java
+++ src/DB02/java/kr/ac/poll/kopo03/dao/VoterDaoImpl.java
@@ -9,7 +9,7 @@ import kr.ac.poll.kopo03.domain.Voter;
 public class VoterDaoImpl implements VoterDao {
 	private static VoterDaoImpl instance = new VoterDaoImpl();
 	
-	private VoterDaoImpl() {
+	VoterDaoImpl() {
 		
 	}
 	
@@ -22,7 +22,7 @@ public class VoterDaoImpl implements VoterDao {
 	
 	@Override
 	public Voter save(Voter vote) {
-		String sql = "insert into tupyo_table values(?, ?)";
+		String sql = "insert into tupyo_table(age, huboId) values(?, ?)";
 
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -36,20 +36,14 @@ public class VoterDaoImpl implements VoterDao {
 		
 		try {
 			conn = DriverManager.getConnection(
-					"jdbc:mysql://192.168.23.27:3306/kopoctc", 
+					"jdbc:mysql://192.168.23.27:3306/pollsystem", 
 					"root", 
 					"kopoctc");
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, vote.getId());
-			pstmt.setInt(2, vote.getAge());
-			pstmt.executeQuery();
-			
-			if (rset.next()) {
-				vote.setId(rset.getInt(1));
-				vote.setAge(rset.getInt(2));
-			} else {
-				throw new SQLException("id가 존재하지 않습니다.");
-			}
+			pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			pstmt.setInt(1, vote.getAge());
+			pstmt.setInt(2, vote.getCandidate().getId());
+			pstmt.executeUpdate();
+			rset = pstmt.getGeneratedKeys();
 			return vote;
 		} catch (Exception e1) {
 			throw new IllegalStateException(e1);
@@ -57,10 +51,9 @@ public class VoterDaoImpl implements VoterDao {
 			close(conn, pstmt, rset);
 		}
 	}
-
 	@Override
-	public int[] selectOne(Candidate can) {		
-		String sql = "select count(age) as cnt from tupyo_table where id = ? group by age order by age";
+	public int[] selectOneCandidateAges(Candidate can) {		
+		String sql = "select count(age) as cnt from tupyo_table where huboId = ? group by age order by age";
 
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -74,7 +67,7 @@ public class VoterDaoImpl implements VoterDao {
 
 		try {
 			conn = DriverManager.getConnection(
-					"jdbc:mysql://192.168.23.27:3306/kopoctc", 
+					"jdbc:mysql://192.168.23.27:3306/pollsystem", 
 					"root", 
 					"kopoctc");
 			pstmt = conn.prepareStatement(sql);
@@ -82,13 +75,13 @@ public class VoterDaoImpl implements VoterDao {
 			
 			rset = pstmt.executeQuery();
 			
-			int[] voteCnt = new int[9];
+			int[] voteAgeCnt = new int[9];
 			while(rset.next()) {
-				for (int cnt : voteCnt) {
-					cnt += rset.getInt("cnt");
+				for (int cnt : voteAgeCnt) {
+					cnt = rset.getInt("cnt");
 				}
 			}
-			return voteCnt;
+			return voteAgeCnt;
 		} catch (Exception e1) {
 			throw new IllegalStateException("존재하지 않는 후보입니다.");
 		} finally {
@@ -97,9 +90,8 @@ public class VoterDaoImpl implements VoterDao {
 	}
 
 	@Override
-	public List<Integer> selectAll() {
-		
-		String sql = "select count(*) as cnt from tupyo_table group by id";
+	public List<Integer> selectAllCandidates() {
+		String sql = "select count(*) as cnt from tupyo_table group by huboId";
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -113,7 +105,7 @@ public class VoterDaoImpl implements VoterDao {
 
 		try {
 			conn = DriverManager.getConnection(
-					"jdbc:mysql://192.168.23.27:3306/kopoctc", 
+					"jdbc:mysql://192.168.23.27:3306/pollsystem", 
 					"root", 
 					"kopoctc");
 			pstmt = conn.prepareStatement(sql);
@@ -122,9 +114,7 @@ public class VoterDaoImpl implements VoterDao {
 
 			List<Integer> voteCnt = new ArrayList<Integer>();
 			while(rset.next()) {
-				for (int cnt : voteCnt) {
-					cnt += rset.getInt("cnt");
-				}
+				voteCnt.add(rset.getInt("cnt"));
 			}
 			return voteCnt;
 		} catch (Exception e1) {
@@ -134,6 +124,115 @@ public class VoterDaoImpl implements VoterDao {
 		}
 	}
 	
+	 public int totalVotesCntOfOneCandidate(Candidate can) {
+		 String sql = "select count(*) as totalCnt from tupyo_table where huboId = ?";
+			
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+
+			Connection conn = null;
+			PreparedStatement pstmt = null;
+			ResultSet rset = null;
+
+			try {
+				conn = DriverManager.getConnection(
+						"jdbc:mysql://192.168.23.27:3306/pollsystem", 
+						"root", 
+						"kopoctc");
+				
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setInt(1, can.getId());
+				
+				rset = pstmt.executeQuery();
+				int totalCnt = 0;
+				while(rset.next()) {
+					totalCnt = rset.getInt("totalCnt");
+				}
+				return totalCnt;
+			} catch (Exception e1) {
+				throw new IllegalStateException(e1);
+			} finally {
+				close(conn, pstmt, rset);
+			}
+	 }
+	
+	 public int totalVotesCntOfAllCandidates() {
+		 String sql = "select count(*) as totalCnt from tupyo_table";
+			
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+
+			Connection conn = null;
+			PreparedStatement pstmt = null;
+			ResultSet rset = null;
+
+			try {
+				conn = DriverManager.getConnection(
+						"jdbc:mysql://192.168.23.27:3306/pollsystem", 
+						"root", 
+						"kopoctc");
+				
+				pstmt = conn.prepareStatement(sql);
+				
+				rset = pstmt.executeQuery();
+				int totalCnt = 0;
+				while(rset.next()) {
+					totalCnt = rset.getInt("totalCnt");
+				}
+				return totalCnt;
+			} catch (Exception e1) {
+				throw new IllegalStateException(e1);
+			} finally {
+				close(conn, pstmt, rset);
+			}
+	 }
+	 
+	 public List<Voter> votersListOfOneCandidate(Candidate can) {
+		List<Voter> voters = new ArrayList<Voter>();
+		Voter voter = new Voter();
+
+		String sql = "SELECT t.id, t.age FROM hubo_table h INNER JOIN tupyo_table t ON h.id = t.huboId and h.id = ?";
+			
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+
+			Connection conn = null;
+			PreparedStatement pstmt = null;
+			ResultSet rset = null;
+
+			try {
+				conn = DriverManager.getConnection(
+						"jdbc:mysql://192.168.23.27:3306/pollsystem", 
+						"root", 
+						"kopoctc");
+				
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setInt(1, can.getId());
+				rset = pstmt.executeQuery();
+
+				while(rset.next()) {
+					voter.setId(rset.getInt("id"));
+					voter.setAge(rset.getInt("age"));
+					voter.setCandidate(can);
+					voters.add(voter);
+				}
+				return voters;
+			} catch (Exception e1) {
+				throw new IllegalStateException(e1);
+			} finally {
+				close(conn, pstmt, rset);
+			}
+	 }
+	 
 	private void close(Connection conn, PreparedStatement pstmt, ResultSet rset) {
 		try {
 			if (rset != null) {
