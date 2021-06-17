f3107a9817e5da8315283faa8dfd4bc2266d1e84
Duyan Kim <46421950+duyankim@users.noreply.github.com>
2021-06-17 20:51:29 +0900

    [fix] **DB02**: `VoterDao.java` modified variable name

--- src/DB02/java/kr/ac/poll/kopo03/dao/VoterDao.java
+++ src/DB02/java/kr/ac/poll/kopo03/dao/VoterDao.java
@@ -8,6 +8,8 @@ import kr.ac.poll.kopo03.domain.Voter;
 
 public interface VoterDao {
 	Voter save(Voter vote) throws Exception;
-	int[] selectOne(Candidate can) throws Exception;
-	List<Integer> selectAll() throws Exception;
+	int[] selectOneCandidateAges(Candidate can) throws Exception;
+	 int totalVotesCntOfOneCandidate(Candidate can) throws Exception;
+	 int totalVotesCntOfAllCandidates() throws Exception;
+	List<Integer> selectAllCandidates() throws Exception;
 }
